79dff620f3d96a30ae0b442473e2efb7f3a3f23f
peter279k <peter279k@gmail.com>
2015-11-28 16:35:18 +0800

    change ajax complete

--- js/get_data.js
+++ js/get_data.js
@@ -1,16 +1,20 @@
 $(function() {
-	$('#main-content').on('lazyshow', function () {
-		$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
-			var str = "";
-			for (var i = data.length - 1; i >= 0; i--) {
-				if(data[i]["message"] === "" || data[i]["object_id"] === "") {
-					continue;
-				}
-				str = "<img data-src='"+"https://mywebservice.info/beautyUniversity/compress_image.php?obj_id="+ data[i]["object_id"] + "&picture_type='thumbnail'"+ "' width='100' height='100'>";
-				$("#main-content").append(str);
-				$(window).lazyLoadXT();
-          				$('#main-content').lazyLoadXT({visibleOnly: false, checkDuplicates: false});
+	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
+		var str = "";
+		for (var i = data.length - 1; i >= 0; i--) {
+			if(data[i]["message"] === "" || data[i]["object_id"] === "") {
+				continue;
 			}
-		});
-	 }).lazyLoadXT({visibleOnly: false});
+			str = "<img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture?type='thumbnail'"+ "' width='100' height='100'>";
+		}
+
+		$("#main-content").append(str);
+     		$('#main-content').lazyLoadXT({visibleOnly: false, checkDuplicates: false});
+	 });
+
+	$(window).on('ajaxComplete', function() {
+  		setTimeout(function() {
+  			$(window).lazyLoadXT();
+  		}, 50);
+	});
 });
