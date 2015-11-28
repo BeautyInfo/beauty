8c4aecbc9060b484dc6f60f5fc7a04c9fb8225c3
peter279k <peter279k@gmail.com>
2015-11-28 13:57:04 +0800

    change parse json fix function

--- js/get_data.js
+++ js/get_data.js
@@ -1,5 +1,6 @@
 $(function() {
-	$.parseJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
-		console.log(data);
+	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
+		var res = $.parseJSON(data);
+		console.log(res[0]["message"]);
 	});
 });
