af97efce69c05ad83d92713afc9fd718dc51cd7b
peter279k <peter279k@gmail.com>
2015-11-28 14:38:56 +0800

    remove src attribute

--- js/get_data.js
+++ js/get_data.js
@@ -2,8 +2,7 @@ $(function() {
 	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
 		var str = "";
 		for (var i = data.length - 1; i >= 0; i--) {
-			str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ 
-				"' src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ "' width='300' height='200' class='lazy-loaded'></p>";
+			str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ "' width='300' height='200' class='lazy-loaded'></p>";
 		}
 
 		$("#main-content").append(str);
