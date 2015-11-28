78e086a2b502e1cd276f1499318d8a987578c8ea
peter279k <peter279k@gmail.com>
2015-11-28 14:51:07 +0800

    add lazload show event

--- js/get_data.js
+++ js/get_data.js
@@ -1,5 +1,5 @@
 $(function() {
-	$('#marker').on('lazyshow', function () {
+	$('#main-content').on('lazyshow', function () {
 		$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
 			var str = "";
 			for (var i = data.length - 1; i >= 0; i--) {
@@ -8,7 +8,7 @@ $(function() {
 
 			$("#main-content").append(str);
 			$(window).lazyLoadXT();
-          			$('#marker').lazyLoadXT({visibleOnly: false, checkDuplicates: false});
+          			$('#main-content').lazyLoadXT({visibleOnly: false, checkDuplicates: false});
 		});
 	 }).lazyLoadXT({visibleOnly: false});
 });
