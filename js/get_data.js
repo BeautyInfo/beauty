9651836d966906719fa8d88ca0901cfc000aaab2
peter279k <peter279k@gmail.com>
2015-11-28 23:18:29 +0800

    add magnific-popup package

--- js/get_data.js
+++ js/get_data.js
@@ -5,10 +5,13 @@ $(function() {
 			if(data[i]["message"] === "" || data[i]["object_id"] === "") {
 				continue;
 			}
+
+			str += "<a href='" + "https://graph.facebook.com/" + data[i]["object_id"] + "/picture?type='normal'" +"'";
 			str += "<img class='shadow-img shadow-img-size' data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture?type='thumbnail'"+ "'>";
+			str += "</a>";
 		}
 
-		$("#main-content").append(str);
+		$(".popup-gallery").append(str);
 	 });
 
 	$(window).on('ajaxComplete', function() {
@@ -16,14 +19,22 @@ $(function() {
   			$(window).lazyLoadXT();
   		}, 50);
 	});
-});
 
-/*
-$( document ).on( "pagecreate", function() {
-	$( ".photopopup" ).on({
-		popupbeforeposition: function() {
-			$( ".photopopup img" ).css( "max-height", "500px" );
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
 		}
 	});
 });
-*/
