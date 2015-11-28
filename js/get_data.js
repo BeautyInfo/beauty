901079386d9da7515ce2a02a15cfb277d0d000f0
peter279k <peter279k@gmail.com>
2015-11-28 15:11:05 +0800

    chane image width,height

--- js/get_data.js
+++ js/get_data.js
@@ -3,7 +3,7 @@ $(function() {
 		$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
 			var str = "";
 			for (var i = data.length - 1; i >= 0; i--) {
-				str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture?type='thumbnail'"+ "' width='300' height='200'></p>";
+				str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture?type='thumbnail'"+ "' width='100' height='100'></p>";
 			}
 
 			$("#main-content").append(str);
