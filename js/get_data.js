5bf896ae895a6c180ba0d76247d1960885b5b60f
peter279k <peter279k@gmail.com>
2015-11-28 21:51:14 +0800

    test image size

--- js/get_data.js
+++ js/get_data.js
@@ -21,7 +21,7 @@ $(function() {
 $( document ).on( "pagecreate", function() {
 	$( ".photopopup" ).on({
 		popupbeforeposition: function() {
-			var maxHeight = $( window ).height() - 60 + "px";
+			var maxHeight = $( window ).height() + 60 + "px";
 			$( ".photopopup img" ).css( "max-height", maxHeight );
 		}
 	});
