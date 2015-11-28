57655c290db5191c97e28133dde94eca2bfe0ad9
peter279k <peter279k@gmail.com>
2015-11-29 06:18:45 +0800

    try to button refresh

--- js/get_data.js
+++ js/get_data.js
@@ -16,7 +16,7 @@ $(function() {
 		event.preventDefault();
 		$("#main-content").html("");
 		$("#main-content").append('<a id="university-btn" onclick="goUniversity()" href="#" data-role="button">表特大學專頁</a>');
-		$("#university-btn").button("refresh");
+		$("#university-btn").button();
 		jsonGet("https://mywebservice.info/beautyUniversity/data_out.php?school=FJU");
 	});
 });
