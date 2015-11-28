e2c3db33e1f786836e6510d31c2028489ab38b53
peter279k <peter279k@gmail.com>
2015-11-28 14:00:17 +0800

    change res to data

--- js/get_data.js
+++ js/get_data.js
@@ -1,6 +1,6 @@
 $(function() {
 	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
-		console.log(res[0]["message"]);
+		console.log(data[0]["message"]);
 		//var res = $.parseJSON(data);
 		//console.log(res[0]["message"]);
 	});
