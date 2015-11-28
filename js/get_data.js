6f47a71419cf64419d6212fd6b88adff677b6b3b
peter279k <peter279k@gmail.com>
2015-11-28 23:37:20 +0800

    remove magnic-popup plugin and add imageLightbox plugin

--- js/get_data.js
+++ js/get_data.js
@@ -6,33 +6,16 @@ $(function() {
 				continue;
 			}
 
-			str += "<a href='" + "https://graph.facebook.com/" + data[i]["object_id"] + "/picture?type='normal'" +"'";
-			str += "<img class='shadow-img shadow-img-size' data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture?type='thumbnail'"+ "'>";
+			str += "<a name='lightbox' href='" + "https://graph.facebook.com/" + data[i]["object_id"] + "/picture?type='normal'" +"'";
+			str += "<img class='imagelightbox shadow-img shadow-img-size' data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture?type='thumbnail'"+ "'>";
 			str += "</a>";
 		}
 
-		$(".popup-gallery").append(str);
-
-		$('.popup-gallery').magnificPopup({
-			delegate: 'a',
-			type: 'image',
-			tLoading: 'Loading image #%curr%...',
-			mainClass: 'mfp-img-mobile',
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-			},
-			image: {
-				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-				titleSrc: function(item) {
-					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-				}
-			}
-		});
+		$("#main-content").append(str);
 	 });
 
 	$(window).on('ajaxComplete', function() {
+		$( 'a[name="lightbox"]' ).imageLightbox();
   		setTimeout(function() {
   			$(window).lazyLoadXT();
   		}, 50);
