2b2a7560a440a4ca3d981bd80896dfd7e6f5025a
peter279k <peter279k@gmail.com>
2015-11-28 15:18:40 +0800

    ignore object_id and message value

--- js/get_data.js
+++ js/get_data.js
@@ -3,6 +3,9 @@ $(function() {
 		$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
 			var str = "";
 			for (var i = data.length - 1; i >= 0; i--) {
+				if(data[i]["message"] === "" || data[i]["object_id"] === "") {
+					continue;
+				}
 				str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture?type='thumbnail'"+ "' width='100' height='100'></p>";
 			}
 
