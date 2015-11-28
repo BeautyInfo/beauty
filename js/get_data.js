23291d202b6013dcce78a83af0690a2cae9807ee
peter279k <peter279k@gmail.com>
2015-11-28 13:58:33 +0800

    debug json

--- js/get_data.js
+++ js/get_data.js
@@ -1,5 +1,6 @@
 $(function() {
 	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
+		console.log(data);
 		var res = $.parseJSON(data);
 		console.log(res[0]["message"]);
 	});
