999ef9f004c4642d5a6f88e5c232a9f5580e4312
peter279k <peter279k@gmail.com>
2015-11-29 04:19:06 +0800

    add button for fju university and add  button events, add jsonGet function

--- js/get_data.js
+++ js/get_data.js
@@ -1,6 +1,34 @@
 $(function() {
 	$( '.swipebox' ).swipebox();
-	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
+	jsonGet("https://mywebservice.info/beautyUniversity/data_out.php?school=university");
+	$(window).on('ajaxComplete', function() {
+  		setTimeout(function() {
+  			$(window).lazyLoadXT();
+  		}, 50);
+	});
+
+	$("#go-top").click(function(event) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+	});
+
+	$("#go-fju").click(function(event) {
+		event.preventDefault();
+		$("#main-content").html("");
+		$("#main-content").append('<a id="go-university" href="#" data-role="button">表特大學專頁</a>');
+		jsonGet("https://mywebservice.info/beautyUniversity/data_out.php?school=FJU");
+	});
+
+	$("#go-university").click(function(event) {
+		event.preventDefault();
+		$("#main-content").html("");
+		$("#main-content").append('<a id="go-fju" href="#" data-role="button">表特輔大專頁</a>');
+		jsonGet("https://mywebservice.info/beautyUniversity/data_out.php?school=university");
+	});
+});
+
+function jsonGet(url) {
+	$.getJSON(url, function(data) {
 		var str = "";
 		for (var i = data.length - 1; i >= 0; i--) {
 			if(data[i]["message"] === "" || data[i]["object_id"] === "") {
@@ -14,15 +42,4 @@ $(function() {
 
 		$("#main-content").append(str);
 	 });
-
-	$(window).on('ajaxComplete', function() {
-  		setTimeout(function() {
-  			$(window).lazyLoadXT();
-  		}, 50);
-	});
-
-	$("#go-top").click(function(event) {
-		event.preventDefault();
-		$("html, body").animate({ scrollTop: 0 }, "slow");
-	});
-});
+}
