a53a94b87b066dfde5723a1a7fb8f2ae27f015f9
peter279k <peter279k@gmail.com>
2015-11-28 14:49:21 +0800

    add image lazyload

--- js/get_data.js
+++ js/get_data.js
@@ -1,10 +1,14 @@
 $(function() {
-	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
-		var str = "";
-		for (var i = data.length - 1; i >= 0; i--) {
-			str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ "' width='300' height='200'></p>";
-		}
+	$('#marker').on('lazyshow', function () {
+		$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
+			var str = "";
+			for (var i = data.length - 1; i >= 0; i--) {
+				str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ "' width='300' height='200'></p>";
+			}
 
-		$("#main-content").append(str);
-	});
+			$("#main-content").append(str);
+			$(window).lazyLoadXT();
+          			$('#marker').lazyLoadXT({visibleOnly: false, checkDuplicates: false});
+		});
+	 }).lazyLoadXT({visibleOnly: false});
 });
