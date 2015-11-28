71de9ff802d42bfc3ea3ce6ff33f24f8c85b42a4
peter279k <peter279k@gmail.com>
2015-11-28 14:30:17 +0800

    add <p></p>

--- js/get_data.js
+++ js/get_data.js
@@ -2,8 +2,8 @@ $(function() {
 	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
 		var str = "";
 		for (var i = data.length - 1; i >= 0; i--) {
-			str += "<img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ 
-				"' src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ "' width='300' height='200' class='lazy-loaded'>";
+			str += "<p><img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ 
+				"' src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ "' width='300' height='200' class='lazy-loaded'></p>";
 		}
 
 		$("#main-content").append(str);
