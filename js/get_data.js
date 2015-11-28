ed1c8ad2274895e0529495bd93aadacffeffa170
peter279k <peter279k@gmail.com>
2015-11-29 06:32:55 +0800

    modify enable button refresh

--- js/get_data.js
+++ js/get_data.js
@@ -16,9 +16,8 @@ $(function() {
 		event.preventDefault();
 		$("#main-content").html("");
 		$("#main-content").append('<a id="university-btn" onclick="goUniversity()" href="#" data-role="button">表特大學專頁</a>');
-		$("#university-btn").button("enable");
-		$("#university-btn").val("表特大學專頁");
 		$("#university-btn").button();
+		$("#university-btn").button("enable");
 		jsonGet("https://mywebservice.info/beautyUniversity/data_out.php?school=FJU");
 	});
 });
