f4c03e2647538d17995a037e5f87f41d2711eb34
peter279k <peter279k@gmail.com>
2015-11-28 14:27:30 +0800

    add image from json data

--- js/get_data.js
+++ js/get_data.js
@@ -1,5 +1,11 @@
 $(function() {
 	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
-		
+		var str = "";
+		for (var i = data.length - 1; i >= 0; i--) {
+			str += "<img data-src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ 
+				"' src='"+"https://graph.facebook.com/"+ data[i]["object_id"] + "/picture"+ "' width='300' height='200' class='lazy-loaded'>";
+		}
+
+		$("#main-content").append(str);
 	});
 });
