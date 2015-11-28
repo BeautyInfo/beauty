d05938cc373189052ff6503d206cba409f889c36
peter279k <peter279k@gmail.com>
2015-11-28 14:04:44 +0800

    add demo for lazy_load_txt

--- js/get_data.js
+++ js/get_data.js
@@ -1,7 +1,5 @@
 $(function() {
 	$.getJSON("https://mywebservice.info/beautyUniversity/data_out.php", function(data) {
-		console.log(data[0]["message"]);
-		//var res = $.parseJSON(data);
-		//console.log(res[0]["message"]);
+		
 	});
 });
