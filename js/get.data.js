31d642a3b50c2bbdafdfc6f8e2774d818dd05aa6
peter279k <peter279k@gmail.com>
2015-12-07 22:09:00 +0800

    try other selector

--- js/get.data.js
+++ js/get.data.js
@@ -8,7 +8,7 @@ $(function() {
 	
 	$( '.swipebox' ).swipebox( {
 		afterOpen: function() {
-			$("div.slide > img").attr("id", "caption");
+			$("div#swipebox-slider > img").attr("name", "caption");
 			$("div.slide > img").captionjs({
 				'class_name'      : 'captionjs', // Class name for each <figure>
 				'schema'          : true,        // Use schema.org markup (i.e., itemtype, itemprop)
