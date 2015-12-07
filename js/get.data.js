82db3ddff79b8d0c8eec0e5f8eb296aa7fa7aa10
peter279k <peter279k@gmail.com>
2015-12-07 21:55:58 +0800

    add swipebox position

--- js/get.data.js
+++ js/get.data.js
@@ -5,22 +5,20 @@ $(function() {
 	
 	var url = "http://mywebservice.info/beautyUniversity/data_out/school/university";
 	
-	$( '.swipebox' ).click(function(e) {
-		e.preventDefault();
-		$( '.swipebox' ).swipebox( {
-			afterOpen: function() {
-				$("div.slide > img").attr("data-caption", "This test data-caption");
-				$('img.caption').captionjs({
-					'class_name'      : 'captionjs', // Class name for each <figure>
-					'schema'          : true,        // Use schema.org markup (i.e., itemtype, itemprop)
-					'mode'            : 'default',   // default | stacked | animated | hide
-					'debug_mode'      : false,       // Output debug info to the JS console
-					'force_dimensions': true,        // Force the dimensions in case they cannot be detected (e.g., image is not yet painted to viewport)
-					'is_responsive'   : false,       // Ensure the figure and image change size when in responsive layout. Requires a container to control responsiveness!
-					'inherit_styles'  : false        // Have the caption.js container inherit box-model properties from the original image
-				});
-			}
-		});
+	
+	$( '.swipebox' ).swipebox( {
+		afterOpen: function() {
+			$("div.slide > img").attr("data-caption", "This test data-caption");
+			$('img.caption').captionjs({
+				'class_name'      : 'captionjs', // Class name for each <figure>
+				'schema'          : true,        // Use schema.org markup (i.e., itemtype, itemprop)
+				'mode'            : 'default',   // default | stacked | animated | hide
+				'debug_mode'      : false,       // Output debug info to the JS console
+				'force_dimensions': true,        // Force the dimensions in case they cannot be detected (e.g., image is not yet painted to viewport)
+				'is_responsive'   : false,       // Ensure the figure and image change size when in responsive layout. Requires a container to control responsiveness!
+				'inherit_styles'  : false        // Have the caption.js container inherit box-model properties from the original image
+			});
+		}
 	});
 	
 	jsonGet(url);
