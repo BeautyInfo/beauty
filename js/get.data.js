4975e676222f908ec80d90fecd5ade7fcf75632e
peter279k <peter279k@gmail.com>
2015-12-07 22:43:45 +0800

    using selector (testing)

--- js/get.data.js
+++ js/get.data.js
@@ -8,14 +8,14 @@ $(function() {
 	
 	$( '.swipebox' ).swipebox( {
 		afterOpen: function() {
-			//console.log($("img[src*='normal']").attr("src"));
-			$(".imagelightbox").captionjs({
+			console.log($("img").attr("name", "caption"));
+			$("img[src*='normal']").captionjs({
 				'class_name'      : 'captionjs', // Class name for each <figure>
 				'schema'          : true,        // Use schema.org markup (i.e., itemtype, itemprop)
 				'mode'            : 'stacked',   // default | stacked | animated | hide
-				'debug_mode'      : true,       // Output debug info to the JS console
+				'debug_mode'      : false,       // Output debug info to the JS console
 				'force_dimensions': true,        // Force the dimensions in case they cannot be detected (e.g., image is not yet painted to viewport)
-				'is_responsive'   : true,       // Ensure the figure and image change size when in responsive layout. Requires a container to control responsiveness!
+				'is_responsive'   : false,       // Ensure the figure and image change size when in responsive layout. Requires a container to control responsiveness!
 				'inherit_styles'  : false        // Have the caption.js container inherit box-model properties from the original image
 			});
 		}
